5a1e8035c2e158eb34e61380bc74bfbe923203c6
Ivaylo Kirilov <iikirilov@gmail.com>
2020-01-27 11:35:37 +0000

    - retrieve private payload from pmt hash - serialize add to group payload - deserialize add to group payload
    Signed-off-by: Ivaylo Kirilov <iikirilov@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/privacy/OnChainPrivacyController.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/privacy/OnChainPrivacyController.java
@@ -14,27 +14,40 @@
  */
 package org.hyperledger.besu.ethereum.privacy;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import org.hyperledger.besu.enclave.types.ReceiveResponse;
 import org.hyperledger.besu.ethereum.chain.Blockchain;
 import org.hyperledger.besu.ethereum.core.Hash;
+import org.hyperledger.besu.ethereum.core.Transaction;
 import org.hyperledger.besu.ethereum.privacy.storage.PrivacyGroupHeadBlockMap;
 import org.hyperledger.besu.ethereum.privacy.storage.PrivateStateStorage;
 import org.hyperledger.besu.ethereum.privacy.storage.PrivateTransactionMetadata;
+import org.hyperledger.besu.ethereum.rlp.BytesValueRLPInput;
+import org.hyperledger.besu.ethereum.rlp.BytesValueRLPOutput;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 
 public class OnChainPrivacyController {
 
-  private final PrivateStateStorage privateStateStorage;
   private final Blockchain blockchain;
+  private final PrivateStateStorage privateStateStorage;
+  private final PrivacyController privacyController;
 
   public OnChainPrivacyController(
-      final Blockchain blockchain, final PrivateStateStorage privateStateStorage) {
+      final Blockchain blockchain,
+      final PrivateStateStorage privateStateStorage,
+      final PrivacyController privacyController) {
     this.blockchain = blockchain;
     this.privateStateStorage = privateStateStorage;
+    this.privacyController = privacyController;
   }
 
   public List<Hash> buildTransactionList(final Bytes32 privacyGroupId) {
@@ -65,5 +78,64 @@ public class OnChainPrivacyController {
       }
     }
     return pmtHashes;
+  }
+
+  public List<PrivateTransaction> retrievePrivateTransactions(
+      final List<Hash> privateMarkerTransactionHashes, final String enclavePublicKey) {
+    final ArrayList<PrivateTransaction> privateTransactions = new ArrayList<>();
+    privateMarkerTransactionHashes.forEach(
+        h -> {
+          final Transaction privateMarkerTransaction =
+              blockchain.getTransactionByHash(h).orElseThrow();
+          final ReceiveResponse receiveResponse =
+              privacyController.retrieveTransaction(
+                  privateMarkerTransaction.getPayload().toBase64String(), enclavePublicKey);
+          final BytesValueRLPInput input =
+              new BytesValueRLPInput(
+                  Bytes.fromBase64String(new String(receiveResponse.getPayload(), UTF_8)), false);
+
+          privateTransactions.add(PrivateTransaction.readFrom(input));
+        });
+    return privateTransactions;
+  }
+
+  public Bytes serializeAddToGroupPayload(
+      final List<Hash> privacyMarkerTransactionHashes,
+      final List<PrivateTransaction> privateTransactions) {
+    if (privacyMarkerTransactionHashes.size() != privateTransactions.size()) {
+      throw new RuntimeException();
+    }
+
+    final BytesValueRLPOutput rlpOutput = new BytesValueRLPOutput();
+    rlpOutput.startList();
+    for (int i = 0; i < privacyMarkerTransactionHashes.size(); ++i) {
+      final Hash hash = privacyMarkerTransactionHashes.get(i);
+      final PrivateTransaction privateTransaction = privateTransactions.get(i);
+      rlpOutput.startList();
+      rlpOutput.writeBytes(hash);
+      privateTransaction.writeTo(rlpOutput);
+      rlpOutput.endList();
+    }
+    rlpOutput.endList();
+
+    return rlpOutput.encoded();
+  }
+
+  public Map<Hash, PrivateTransaction> deserializeAddToGroupPayload(
+      final Bytes encodedAddToGroupPayload) {
+    final HashMap<Hash, PrivateTransaction> deserializedResponse = new HashMap<>();
+    final BytesValueRLPInput bytesValueRLPInput =
+        new BytesValueRLPInput(encodedAddToGroupPayload, false);
+    final int noOfEntries = bytesValueRLPInput.enterList();
+    for (int i = 0; i < noOfEntries; i++) {
+      bytesValueRLPInput.enterList();
+      final Hash privacyMarkerTransactionHash = Hash.wrap(bytesValueRLPInput.readBytes32());
+      final PrivateTransaction privateTransaction =
+          PrivateTransaction.readFrom(bytesValueRLPInput.readAsRlp());
+      deserializedResponse.put(privacyMarkerTransactionHash, privateTransaction);
+      bytesValueRLPInput.leaveList();
+    }
+    bytesValueRLPInput.leaveList();
+    return deserializedResponse;
   }
 }
--- ethereum/core/src/test/java/org/hyperledger/besu/ethereum/privacy/OnChainPrivacyControllerTest.java
+++ ethereum/core/src/test/java/org/hyperledger/besu/ethereum/privacy/OnChainPrivacyControllerTest.java
@@ -24,19 +24,29 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import org.hyperledger.besu.crypto.SECP256K1;
+import org.hyperledger.besu.enclave.types.ReceiveResponse;
 import org.hyperledger.besu.ethereum.chain.Blockchain;
+import org.hyperledger.besu.ethereum.core.Address;
 import org.hyperledger.besu.ethereum.core.BlockHeader;
 import org.hyperledger.besu.ethereum.core.Hash;
+import org.hyperledger.besu.ethereum.core.Transaction;
+import org.hyperledger.besu.ethereum.core.Wei;
 import org.hyperledger.besu.ethereum.privacy.storage.PrivacyGroupHeadBlockMap;
 import org.hyperledger.besu.ethereum.privacy.storage.PrivateBlockMetadata;
 import org.hyperledger.besu.ethereum.privacy.storage.PrivateStateStorage;
 import org.hyperledger.besu.ethereum.privacy.storage.PrivateTransactionMetadata;
+import org.hyperledger.besu.ethereum.rlp.RLP;
 
+import java.math.BigInteger;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import com.google.common.collect.Lists;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
+import org.apache.tuweni.io.Base64;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Before;
 import org.junit.Test;
@@ -46,19 +56,112 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class OnChainPrivacyControllerTest {
 
+  private static final String TRANSACTION_KEY = "93Ky7lXwFkMc7+ckoFgUMku5bpr9tz4zhmWmk9RlNng=";
+  private static final SECP256K1.KeyPair KEY_PAIR =
+      SECP256K1.KeyPair.create(
+          SECP256K1.PrivateKey.create(
+              new BigInteger(
+                  "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63", 16)));
+
   private static final String ENCLAVE_PUBLIC_KEY = "A1aVtMxLCUHmBVHXoZzzBgPbW/wj5axDpW9X8l91SGo=";
   private static final String PRIVACY_GROUP_ID = "DyAOiF/ynpc+JXa2YAGB0bCitSlOMNm+ShmB/7M6C4w=";
+
+  private static final Transaction PUBLIC_TRANSACTION =
+      Transaction.builder()
+          .nonce(0)
+          .gasPrice(Wei.of(1000))
+          .gasLimit(3000000)
+          .to(Address.fromHexString("0x627306090abab3a6e1400e9345bc60c78a8bef57"))
+          .value(Wei.ZERO)
+          .payload(Base64.decode(TRANSACTION_KEY))
+          .sender(Address.fromHexString("0xfe3b557e8fb62b89f4916b721be55ceb828dbd73"))
+          .chainId(BigInteger.valueOf(2018))
+          .signAndBuild(KEY_PAIR);
+
+  private static final PrivateTransaction VALID_PRIVATE_TRANSACTION =
+      new PrivateTransaction(
+          0L,
+          Wei.of(1),
+          21000L,
+          Optional.of(
+              Address.wrap(Bytes.fromHexString("0x095e7baea6a6c7c4c2dfeb977efac326af552d87"))),
+          Wei.of(
+              new BigInteger(
+                  "115792089237316195423570985008687907853269984665640564039457584007913129639935")),
+          SECP256K1.Signature.create(
+              new BigInteger(
+                  "32886959230931919120748662916110619501838190146643992583529828535682419954515"),
+              new BigInteger(
+                  "14473701025599600909210599917245952381483216609124029382871721729679842002948"),
+              Byte.valueOf("0")),
+          Bytes.fromHexString("0x"),
+          Address.wrap(Bytes.fromHexString("0x83db8f1f96dbf773d2d719f70dc89e5c772cddbc")),
+          Optional.empty(),
+          Bytes.fromBase64String("A1aVtMxLCUHmBVHXoZzzBgPbW/wj5axDpW9X8l91SGo="),
+          Optional.of(
+              Lists.newArrayList(
+                  Bytes.fromBase64String("A1aVtMxLCUHmBVHXoZzzBgPbW/wj5axDpW9X8l91SGo="),
+                  Bytes.fromBase64String("Ko2bVqD+nNlNYL5EE7y3IdOnviftjiizpjRt+HTuFBs="))),
+          Optional.empty(),
+          Restriction.RESTRICTED);
 
   private OnChainPrivacyController onChainPrivacyController;
   private Blockchain blockchain;
   private PrivateStateStorage privateStateStorage;
+  private PrivacyController privacyController;
 
   @Before
   public void setUp() {
     blockchain = mock(Blockchain.class);
     privateStateStorage = mock(PrivateStateStorage.class);
+    privacyController = mock(PrivacyController.class);
 
-    onChainPrivacyController = new OnChainPrivacyController(blockchain, privateStateStorage);
+    onChainPrivacyController =
+        new OnChainPrivacyController(blockchain, privateStateStorage, privacyController);
+  }
+
+  @Test
+  public void canRetrievePrivateTransactions() {
+    when(blockchain.getTransactionByHash(any(Hash.class)))
+        .thenReturn(Optional.of(PUBLIC_TRANSACTION));
+    when(privacyController.retrieveTransaction(any(String.class), any(String.class)))
+        .thenReturn(
+            new ReceiveResponse(
+                RLP.encode(VALID_PRIVATE_TRANSACTION::writeTo).toBase64String().getBytes(),
+                "",
+                ""));
+
+    final List<PrivateTransaction> privateTransactions =
+        onChainPrivacyController.retrievePrivateTransactions(singletonList(Hash.ZERO), "");
+
+    assertThat(privateTransactions.size()).isEqualTo(1);
+    assertThat(privateTransactions.get(0))
+        .isEqualToComparingFieldByField(VALID_PRIVATE_TRANSACTION);
+  }
+
+  @Test
+  public void canSerializeAddToGroupPayload() {
+    final Bytes expected =
+        Bytes.fromHexString(
+            "0xf90115f90112a00000000000000000000000000000000000000000000000000000000000000000f8ef800182520894095e7baea6a6c7c4c2dfeb977efac326af552d87a0ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff801ba048b55bfa915ac795c431978d8a6a992b628d557da5ff759b307d495a36649353a01fffd310ac743f371de3b9f7f9cb56c0b28ad43601b4ab949f53faa07bd2c804a0035695b4cc4b0941e60551d7a19cf30603db5bfc23e5ac43a56f57f25f75486af842a0035695b4cc4b0941e60551d7a19cf30603db5bfc23e5ac43a56f57f25f75486aa02a8d9b56a0fe9cd94d60be4413bcb721d3a7be27ed8e28b3a6346df874ee141b8a72657374726963746564");
+
+    final Bytes encoded =
+        onChainPrivacyController.serializeAddToGroupPayload(
+            singletonList(Hash.ZERO), singletonList(VALID_PRIVATE_TRANSACTION));
+
+    assertThat(encoded).isEqualTo(expected);
+  }
+
+  @Test
+  public void canDeserializeAddToGroupPayload() {
+    final Bytes encoded =
+        onChainPrivacyController.serializeAddToGroupPayload(
+            singletonList(Hash.ZERO), singletonList(VALID_PRIVATE_TRANSACTION));
+
+    final Map<Hash, PrivateTransaction> decoded =
+        onChainPrivacyController.deserializeAddToGroupPayload(encoded);
+
+    assertThat(decoded).isEqualTo(singletonMap(Hash.ZERO, VALID_PRIVATE_TRANSACTION));
   }
 
   @Test
